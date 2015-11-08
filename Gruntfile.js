283fccf9a9416d31ed2a812c24bbdc36a7876520
Gabby Losch <glosch00@gmail.com>
2015-11-08 15:34:04 -0500

    removed comments, Grunt compiles single js for index.haml to consume, README restored

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,8 +18,6 @@ module.exports = function(grunt) {
         curly: true,
         eqeqeq: true,
         maxdepth: 2,
-        //undef: true, // NOTE: use this later
-        //unused: true,
         quotmark: "double",
         globals: {
           jQuery: true
@@ -48,9 +46,9 @@ module.exports = function(grunt) {
       app: {
         src: ["js/*.js",
           "js/config/" + "<%= configs.target %>" + ".js",
-          "js/factories/**/*.js",
-          "js/directives/**/*.js",
-          "js/controllers/**/*.js",
+          "js/factories/*.js",
+          "js/directives/*.js",
+          "js/controllers/*.js",
         ],
         // the location of the resulting JS file
         dest: "public/js/app.js"
--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,1 @@
-// google.maps.event.addDomListener(window, 'load', function(){
-//   var properties = {
-//     center:new google.maps.LatLng(40.7293866,-73.9275324),
-//     zoom:10,
-//     mapTypeId:google.maps.MapTypeId.ROADMAP
-//   };
-//   var map = new google.maps.Map(document.getElementById("googleMap"), properties);
-
-// 	var marker = new google.maps.Marker({
-// 	  position: new google.maps.LatLng(40.7293866,-73.9275324),
-// 	});
-
-// 	marker.setMap(map);
-// });
+var citibikeMap = angular.module('citibikeMap',[]);
